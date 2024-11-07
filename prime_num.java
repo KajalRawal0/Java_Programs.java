b51874b3ecf2cb509cf71fdadc99a59ead7b4739
Kajal Rawal <kajalrawal132@gmail.com>
2024-11-07 18:28:00 +0530

    Update prime_num.java

--- prime_num.java
+++ prime_num.java
@@ -1,26 +1,5 @@
 import java.util.Scanner;
 
-// public class prime_num {
-    
-
-//     static boolean isPrime(int n){
-//         for(int i=2;i<n/2;i++){
-//             if(n%i==0){
-//                 return false;
-//             }
-//             return true;
-//         }
-
-//     }
-//     public static void main(String[] args) {
-//         // Scanner sc = new Scanner(System.in);
- 
-//         // System.out.print("Enter a number:");
-//         // int n=sc.nextInt();
-//         System.out.println(isPrime(41));
-//     }
-// }import java.util.Scanner;
-
 public class prime_num {
 
     static boolean isPrime(int n) {
